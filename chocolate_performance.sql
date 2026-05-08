accd9ac333ff27b19f0489429aa1a1572d2e3644
fatima18525-cloud <fatima18525@gmail.com>
2026-05-09 01:56:17 +0300

    I have created a ctas

--- chocolate_performance.sql
+++ chocolate_performance.sql
@@ -1,28 +1,16 @@
 ----Stores Preformance
-SELECT * 
-FROM chocolate_store_sales;
+
 ------------
 SELECT 
-CASE city
-WHEN 'Toronto' THEN  'Canada'
-WHEN 'Berlin' THEN 'Germany'
-WHEN 'Paris' THEN 'France'
-WHEN 'New York' THEN 'USA'
-WHEN 'London' THEN 'UK'
-ELSE 'Australia'
-end   The_country,
-city, store_name,profits
+	CASE city
+	WHEN 'Toronto' THEN  'Canada'
+	WHEN 'Berlin' THEN 'Germany'
+	WHEN 'Paris' THEN 'France'
+	WHEN 'New York' THEN 'USA'
+	WHEN 'London' THEN 'UK'
+	ELSE 'Australia'
+	end   The_country,*
+	INTO chocolate_2_sales
+	FROM  chocolate_store_sales
 
-FROM (SELECT 
-	t.country , t.city,
-	t.store_name,
-	ROUND(SUM(s.profit),2) profits
-FROM sales s
-LEFT JOIN stores t
-ON s.store_id = t.store_id
-GROUP BY t.country , t.city,
-	t.store_name
-)t
 
-SELECT TOP 10*
-FROM stores
